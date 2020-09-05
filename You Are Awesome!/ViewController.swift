8f0e2d47624a094a0d49cc847e53ae89dfc48513
Jennifer Joseph <josephjz@bc.edu>
2020-09-05 14:54:03 -0400

    Code for Video Lesson 2.3. Incorporated Touch Up Inside Button with Display Message by Linking Interface Elements to Code with an @IBOutlet and @IBAction

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+         print("😎  viewDidLoad() has run!")
+        messageLabel.text = "Fabulous? That's you!"
     }
 
-
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        print("🤠  The message button was pressed!")
+        messageLabel.text = "You are awesome!"
+    }
+    
 }
-
+   
+  
