a065eba78eb1e4ceddcaa4de5c93dc933da65160
Jennifer Joseph <josephjz@bc.edu>
2020-09-07 11:59:53 -0400

    Completed 2.9 : working with String Interpolation and int data type

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -11,8 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var messageLabel: UILabel!
+    
+    var imageNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,30 +22,36 @@ class ViewController: UIViewController {
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
+        //        print("🤠  The message button was pressed!")
+        //        let awesomeMessage = "You are awesome!"
+        //        let greatMessage = "You are great!"
+        //        let bombMessage = "You are bomb!"
+        //
+        //        if messageLabel.text == awesomeMessage {
+        //            messageLabel.text = greatMessage
+        //            imageView.image = UIImage(named: "image1")
+        //        }
+        //        else if messageLabel.text == greatMessage {
+        //            messageLabel.text = bombMessage
+        //            imageView.image = UIImage(named: "image2")
+        //        }
+        //        else {
+        //            messageLabel.text = awesomeMessage
+        //            imageView.image = UIImage(named: "image0")
+        //        }
+                
+        //        messageLabel.text = "You are awesome!"
+        //        imageView.image = UIImage(named: "image0")
         
-        //print("🤠  The message button was pressed!")
-        
-        let awesomeMessage = "You are awesome!"
-        let greatMessage = "You are great!"
-        let bombMessage = "You are bomb!"
-
-        if messageLabel.text == awesomeMessage {
-            messageLabel.text = greatMessage
-            imageView.image = UIImage(named: "image1")
-        }
-            
-        else if messageLabel.text == greatMessage {
-            messageLabel.text = bombMessage
-            imageView.image = UIImage(named: "image2")
-        }
-            
-        else {
-            messageLabel.text = awesomeMessage
-            imageView.image = UIImage(named: "image0")
+        print(imageNumber)
+        //let imageName = "image" + String(imageNumber)
+        let imageName = "image\(imageNumber)" // string interpolation, anything that goes in () will be type casted to string
+        imageView.image = UIImage(named: imageName)
+        imageNumber = imageNumber + 1
+        if imageNumber == 10 {
+            imageNumber = 0
         }
         
-        //messageLabel.text = "You are awesome!"
-        //imageView.image = UIImage(named: "image0")
     }
 }
    
